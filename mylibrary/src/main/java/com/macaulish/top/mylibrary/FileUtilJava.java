e8926ec2a9c85e46d4d3642e41469e1a3f7287bb
macaulishchina <huyidongchina@Gmail.com>
2018-07-05 21:45:06 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/macaulish/top/mylibrary/FileUtilJava.java
@@ -0,0 +1,13 @@
+package com.macaulish.top.mylibrary;
+
+/**
+ * created by hu
+ * at 2018/7/5
+ * in project ApplicationTest
+ * description
+ */
+public class FileUtilJava {
+    public static String test(){
+        return "test";
+    }
+}
